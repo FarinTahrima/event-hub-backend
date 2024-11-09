5be8ecbcab92b340548b3a29c19d9985392e9891
farin.rahman <farin.rahman@fdmgroup.com>
2024-11-10 02:04:05 +0800

    host-manage-event: add/remove videos and content to event

--- src/main/java/com/fdmgroup/backend_eventhub/modules/configuration/WebConfig.java
+++ src/main/java/com/fdmgroup/backend_eventhub/modules/configuration/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/video/**")
+        registry.addResourceHandler("/videos/**")
                 .addResourceLocations(VIDEO_DIRECTORY_PATH);
     }
 
