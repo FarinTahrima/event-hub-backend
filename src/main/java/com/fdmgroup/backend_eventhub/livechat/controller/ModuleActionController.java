4d33335d30d18b1375a562d95b259fd16f77ff88
Timothy Chai <159506546+tchai17@users.noreply.github.com>
2024-11-19 10:41:29 +0800

    :sparkles: fix(livechat): add get current module endpoint
    This commit adds a new endpoint `/api/moduleAction/{sessionID}` to retrieve the current module action.  It also handles cases where the module action is not found, returning a 400 Bad Request response.  This addresses an issue where client-side logic was failing due to missing data.

--- src/main/java/com/fdmgroup/backend_eventhub/livechat/controller/ModuleActionController.java
+++ src/main/java/com/fdmgroup/backend_eventhub/livechat/controller/ModuleActionController.java
@@ -1,14 +1,23 @@
 package com.fdmgroup.backend_eventhub.livechat.controller;
 
 import com.fdmgroup.backend_eventhub.livechat.models.ModuleAction;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class ModuleActionController {
 
     SimpMessagingTemplate template;
+
+    Map<String, ModuleAction> currentModuleMap = new ConcurrentHashMap<>();
 
     public ModuleActionController(SimpMessagingTemplate template) {
         this.template = template;
@@ -18,6 +27,19 @@ public class ModuleActionController {
     public void triggerModuleAction(ModuleAction action) {
         System.out.println(action);
         template.convertAndSend("/topic/moduleAction/" + action.SESSION_ID(), action);
+        if ( !action.TYPE().equals("poll_vote") ) {
+            currentModuleMap.put(action.SESSION_ID(), action);
+        }
+    }
 
+    @GetMapping("/api/moduleAction/{sessionID}")
+    public ResponseEntity<?> getCurrentModule(@PathVariable String sessionID) {
+        try {
+            ModuleAction action = currentModuleMap.get(sessionID);
+            return ResponseEntity.ok(action);
+        } catch ( Exception e ) {
+            System.out.println(sessionID + " is not found");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sessionID + " is not found");
+        }
     }
 }
