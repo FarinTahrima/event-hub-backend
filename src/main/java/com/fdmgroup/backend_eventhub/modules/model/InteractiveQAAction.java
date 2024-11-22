26ab29ca077e7c4b48ca964bcae0112fd6798dbf
farin.rahman <farin.rahman@fdmgroup.com>
2024-11-22 23:18:20 +0800

    bug-fixes: fixed the moderation bug

--- src/main/java/com/fdmgroup/backend_eventhub/modules/model/InteractiveQAAction.java
+++ src/main/java/com/fdmgroup/backend_eventhub/modules/model/InteractiveQAAction.java
@@ -2,5 +2,5 @@ package com.fdmgroup.backend_eventhub.modules.model;
 
 import com.fdmgroup.backend_eventhub.modules.dto.InteractiveQuestion;
 
-public record InteractiveQAAction(String SESSION_ID, String TYPE, String QUESTION, String TEXT) {
+public record InteractiveQAAction(String SESSION_ID, String TYPE, String QUESTION) {
 }
