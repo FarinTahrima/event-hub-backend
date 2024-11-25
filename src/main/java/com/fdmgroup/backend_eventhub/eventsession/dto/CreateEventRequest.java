7890f1696cb1176010935a9c12c73317ac126917
Timothy Chai <159506546+tchai17@users.noreply.github.com>
2024-11-25 10:43:35 +0800

    ✨refactor(dto): simplify DTO classes by using Lombok annotations
    Removed redundant getter and setter methods from various DTO classes
    and replaced them with Lombok's `@Getter` and `@Setter` annotations.
    This change improves code readability and reduces boilerplate code
    in the `JoinEventResponse`, `JoinEventRequest`, `UpdateEventPasswordRequest`,
    `UpdateEventRequest`, `CreateEventResponse`, `CreateEventRequest`,
    and `UploadImageRequest` classes.

--- src/main/java/com/fdmgroup/backend_eventhub/eventsession/dto/CreateEventRequest.java
+++ src/main/java/com/fdmgroup/backend_eventhub/eventsession/dto/CreateEventRequest.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+
 @Setter
 @Getter
 public class CreateEventRequest {
@@ -14,46 +15,6 @@ public class CreateEventRequest {
     private String password;
     private LocalDate scheduledDate;
     private LocalTime scheduledTime;
-
-    public String getEventName() {
-        return eventName;
-    }
-
-    public void setEventName(String eventName) {
-        this.eventName = eventName;
-    }
-
-    public Long getAccountID() {
-        return accountID;
-    }
-
-    public void setAccountID(Long accountID) {
-        this.accountID = accountID;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDate getScheduledDate() {
-        return scheduledDate;
-    }
-
-    public void setScheduledDate(LocalDate scheduledDate) {
-        this.scheduledDate = scheduledDate;
-    }
-
-    public LocalTime getScheduledTime() {
-        return scheduledTime;
-    }
-
-    public void setScheduledTime(LocalTime scheduledTime) {
-        this.scheduledTime = scheduledTime;
-    }
 
     @Override
     public String toString() {
